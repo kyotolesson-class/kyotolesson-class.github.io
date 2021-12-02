b4f7401f4e71a105d6e9e81a53076f6e92558cb7
ksdavidc <62230004+ksdavidc@users.noreply.github.com>
2021-12-03 06:40:07 +0900

    added lazy viewing ot zak viewer

--- assets/myjquerycode.js
+++ assets/myjquerycode.js
@@ -1,189 +1,252 @@
-        $(document).ready(function() {
+$(document).ready(function () {
 
 
-            // TOC SECTION    
-            // if mainmenu item is clicked
-            // makes sure details is open before inner click is applied, so link works.
-            // Also toggle details but don't follow links on outermost links (for mobile)
-            // and throw error if state is unmatched
-            // RECENTLY FIXED BUGGY??: works for links to details within details
-            $("#toc ul li a").on("click", function(event) {
+	// implements lazy loading of zak iframes to speed page load.
+    $("button[name='insertZak'], div[name='insertZak'], a[name='insertZak']").one("click", function () {
+		var domElement = $(
+			'<iframe src="https://scratch-viewer.techlit.app/view#project=' + $(this).attr("projectID") + '&backgroundColor=#ffec9f&width=515&autoStart=true" width="482" height="393" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+		)
 
-                //if closed and inner link open main details first then trigger click
-                if (!$($(this).attr("href")).parents("details").last().prop("open") && $(this).is("ul li ul li a")) {
-                    // open main details for this
-                    $($(this).attr("href")).parents("details").last().children("summary").first().trigger("click");
-                    // trigger link only if not to a details tag, or if to a inner details tag (e.g. "details" details") and this details is not open
-                    if (!$($(this).attr("href")).parent().parent().is("details") || ($($(this).attr("href")).parent().parent().is("details") && !$($(this).attr("href")).parent().parent().prop("open"))) {
-                        $($(this).attr("href")).trigger("click");
-                    };
-                    return true;
-                };
+		$(this).replaceWith(domElement);
+	});
 
-                //if open and inner link just trigger click 
-                if ($($(this).attr("href")).parents("details").last().prop("open") && $(this).is("ul li ul li a")) {
-                    // trigger link only if not to a details tag, or if to a inner details tag (e.g. "details" details") and this details is not open
-                    if (!$($(this).attr("href")).parent().parent().is("details") || ($($(this).attr("href")).parent().parent().is("details") && !$($(this).attr("href")).parent().parent().prop("open"))) {
-                        $($(this).attr("href")).trigger("click");
-                    };
-                    return true;
-                };
+	// TOC SECTION
+	// if mainmenu item is clicked
+	// makes sure details is open before inner click is applied, so link works.
+	// Also toggle details but don't follow links on outermost links (for mobile)
+	// and throw error if state is unmatched
+	// RECENTLY FIXED BUGGY??: works for links to details within details
+	$("#toc ul li a").on("click", function (event) {
+		//if closed and inner link open main details first then trigger click
+		if (
+			!$($(this).attr("href")).parents("details").last().prop("open") &&
+			$(this).is("ul li ul li a")
+		) {
+			// open main details for this
+			$($(this).attr("href"))
+				.parents("details")
+				.last()
+				.children("summary")
+				.first()
+				.trigger("click");
+			// trigger link only if not to a details tag, or if to a inner details tag (e.g. "details" details") and this details is not open
+			if (
+				!$($(this).attr("href")).parent().parent().is("details") ||
+				($($(this).attr("href")).parent().parent().is("details") &&
+					!$($(this).attr("href")).parent().parent().prop("open"))
+			) {
+				$($(this).attr("href")).trigger("click");
+			}
+			return true;
+		}
 
+		//if open and inner link just trigger click
+		if (
+			$($(this).attr("href")).parents("details").last().prop("open") &&
+			$(this).is("ul li ul li a")
+		) {
+			// trigger link only if not to a details tag, or if to a inner details tag (e.g. "details" details") and this details is not open
+			if (
+				!$($(this).attr("href")).parent().parent().is("details") ||
+				($($(this).attr("href")).parent().parent().is("details") &&
+					!$($(this).attr("href")).parent().parent().prop("open"))
+			) {
+				$($(this).attr("href")).trigger("click");
+			}
+			return true;
+		}
 
-                //if open and outer link close and don't follow link.
-                if ($($(this).attr("href")).parents("details").last().prop("open") && !$(this).is("ul li ul li a")) {
-                    // checks toggle state
-                    // toggled, then close and don't follow
-                    // else if not toggled, throw error
-                    if ($(this).parents("li").last().children("a").first().hasClass("TOCtoggled")) {
-                        $($(this).attr("href")).parents("details").last().removeAttr("open");
-                    } else {
-                        alert("This is a minor error (unintended menu state). Please notify webmaster.")
-                    }
-                    // don't follow link
-                    return false;
-                };
+		//if open and outer link close and don't follow link.
+		if (
+			$($(this).attr("href")).parents("details").last().prop("open") &&
+			!$(this).is("ul li ul li a")
+		) {
+			// checks toggle state
+			// toggled, then close and don't follow
+			// else if not toggled, throw error
+			if (
+				$(this)
+					.parents("li")
+					.last()
+					.children("a")
+					.first()
+					.hasClass("TOCtoggled")
+			) {
+				$($(this).attr("href"))
+					.parents("details")
+					.last()
+					.removeAttr("open");
+			} else {
+				alert(
+					"This is a minor error (unintended menu state). Please notify webmaster."
+				);
+			}
+			// don't follow link
+			return false;
+		}
 
-                //if closed and outer link open details don't follow link (for mobile users)
-                if (!$($(this).attr("href")).parents("details").last().prop("open") && !$(this).is("ul li ul li a")) {
-                    //open without clicking
-                    $($(this).attr("href")).parents("details").last().attr("open", "open")
-                        // don't follow link
-                    return false;
-                };
+		//if closed and outer link open details don't follow link (for mobile users)
+		if (
+			!$($(this).attr("href")).parents("details").last().prop("open") &&
+			!$(this).is("ul li ul li a")
+		) {
+			//open without clicking
+			$($(this).attr("href"))
+				.parents("details")
+				.last()
+				.attr("open", "open");
+			// don't follow link
+			return false;
+		}
+	});
+	// END TOC SECTION
 
-            });
-            // END TOC SECTION
+	// NAV SECTION
 
-            // NAV SECTION
+	// on first run, hides nav submenus until clicked.
+	$("nav .tag-h3").toggle();
 
-            // on first run, hides nav submenus until clicked.
-            $("nav .tag-h3").toggle();
+	// removes identical tags. This needs revision
+	var previousVal = "Initial";
+	var previousItem = "";
+	$($("nav li.tag-h2").get().reverse()).each(function (i, item) {
+		var currentVal = $(item).text(); //alert(currentVal + i);
+		var currentItem = $(item);
+		if (currentVal == previousVal) {
+			//alert(currentVal + "==" + previousVal);
+			//$(item).toggle();
+			currentItem.toggleClass("navIndented ");
+			previousItem.toggle();
+			//alert("removed");
+		}
+		previousVal = currentVal;
+		previousItem = currentItem;
+	});
 
+	// if nav item is clicked, makes sure details is open before click
+	// is applied, so link works.
+	$("nav ul li a").on("click", function (event) {
+		//if closed and inner tag-h3 link open details first
+		if (
+			!$($(this).attr("href")).parents("details").last().prop("open") &&
+			$(this).is("nav .tag-h3 a")
+		) {
+			$($(this).attr("href"))
+				.parents("details")
+				.last()
+				.children("summary")
+				.first()
+				.trigger("click");
+		}
+		$($(this).attr("href")).trigger("click");
+	});
 
-            // removes identical tags. This needs revision
-            var previousVal = 'Initial';
-            var previousItem = '';
-            $($('nav li.tag-h2').get().reverse()).each(function (i, item) {
-                var currentVal = $(item).text(); //alert(currentVal + i);
-                var currentItem = $(item);
-                if (currentVal == previousVal) { //alert(currentVal + "==" + previousVal);
-                    //$(item).toggle();
-                    currentItem.toggleClass("navIndented ");
-                    previousItem.toggle();
-                    //alert("removed");
-                }
-                previousVal = currentVal;
-                previousItem = currentItem;
-            });
+	// shows tag-h3 when tag-h2 is hovered. Using hoverintent.js with interval set to 200 to make it slower.
+	$("nav .tag-h2 a").hoverIntent({
+		over: function () {
+			// shows contiguous tag-h3. Have to skip the extra tag-h2 created by details
+			// by finding next .tag-h3, then selecting contiguous
+			$(this)
+				.parent()
+				.nextUntil(".tag-h3")
+				.nextUntil(".tag-h2")
+				.toggle("slow", "swing");
+		},
+		out: function () {},
+		interval: 200,
+	});
+	// END NAV SECTION
 
+	// DETAILS SECTION
 
-            // if nav item is clicked, makes sure details is open before click
-            // is applied, so link works.
-            $("nav ul li a").on("click", function(event) {
+	// open top details on first run
+	$("details").first().children("summary").first().trigger("click");
 
-                //if closed and inner tag-h3 link open details first
-                if (!$($(this).attr("href")).parents("details").last().prop("open") && $(this).is("nav .tag-h3 a")) {
-                    $($(this).attr("href")).parents("details").last().children("summary").first().trigger("click");
-                };
-                $($(this).attr("href")).trigger("click");
-            });
+	// This toggles details when border is clicked
+	$("details").click(function (e) {
+		//first test to make sure it is not a youtube image that is being clicked, as firefox closes the details in this instance for some unknown reason
+		if (!e.target.classList.contains("youtubeimage")) {
+			//now, test to see if border is being clicked
+			if (e.offsetX <= parseInt($(this).css("borderLeftWidth"))) {
+				// needed or parent of details details gets closed too.
+				event.stopPropagation();
+				// now you can trigger the click
+				$(this).children("summary").first().trigger("click");
+			}
+		}
+	});
 
-            // shows tag-h3 when tag-h2 is hovered. Using hoverintent.js with interval set to 200 to make it slower.
-            $("nav .tag-h2 a").hoverIntent({
-            over: function() {
-                // shows contiguous tag-h3. Have to skip the extra tag-h2 created by details
-                // by finding next .tag-h3, then selecting contiguous
-                $(this).parent().nextUntil(".tag-h3").nextUntil(".tag-h2").toggle("slow", "swing");
-            }, out: function() {} , interval: 200 });
-            // END NAV SECTION
+	// This toggles both menu elements when details state changes by finding links to detail h2 ids.
+	// only details with h2 ids do this. This may need a tweak.
+	$("details").on("toggle", function () {
+		//alert("about to  toggle");
+		var $elementToToggle =
+			"[href='#" + $(this).children("h2").first().attr("id") + "']";
+		$($elementToToggle).toggleClass("TOCtoggled");
+		//alert("details toggled" + $(this).css('borderLeftWidth') + $(this).html());
+	});
 
-            // DETAILS SECTION
+	// .innerlink class allows links in one main details to open links within another main details even if that main details is closed.
+	$(".innerlink").on("click", function (event) {
+		//if closed and inner link open details first then trigger click
+		// TODO: Not checked for links to details within details
+		if (!$($(this).attr("href")).parents("details").last().prop("open")) {
+			$($(this).attr("href"))
+				.parents("details")
+				.last()
+				.find("summary")
+				.trigger("click");
+			$($(this).attr("href")).trigger("click");
+		}
+		//alert("2");
+	});
+	// END DETAILS SECTION
 
-            // open top details on first run
-            $("details").first().children("summary").first().trigger("click")
+	// ANCHOR SECTION modify anchors
 
-            // This toggles details when border is clicked
-            $("details").click(function(e) {
-                //first test to make sure it is not a youtube image that is being clicked, as firefox closes the details in this instance for some unknown reason
-                if (!e.target.classList.contains("youtubeimage")) {
-                    //now, test to see if border is being clicked
-                    if (e.offsetX <= parseInt($(this).css('borderLeftWidth'))) {
-                        // needed or parent of details details gets closed too.
-                        event.stopPropagation();
-                        // now you can trigger the click 
-                        $(this).children("summary").first().trigger("click");
-                    };
-                };
-            });
+	// Don't follow empty links, like footnotes
+	$('a[href="#"]').on("click", function (event) {
+		//alert("3");
+		return false;
+	});
 
-            // This toggles both menu elements when details state changes by finding links to detail h2 ids.
-            // only details with h2 ids do this. This may need a tweak.
-            $('details').on('toggle', function() {
-                //alert("about to  toggle");
-                var $elementToToggle = "[href='#" + $(this).children("h2").first().attr("id") + "']";
-                $($elementToToggle).toggleClass("TOCtoggled");
-                //alert("details toggled" + $(this).css('borderLeftWidth') + $(this).html());
-            });
+	// /* when link to a target in the page, i.e. #.. is clicked, the link is highlighted via jquery. Used to show which picture is showing in the galleries. see style sheet a.clickeditem */
+	$('a[href*="#"]').click(function () {
+		//alert("clicked" + $(this).attr("href") );
+		//$(this).attr("href");
+		var linktoclick = $("a[href='" + $(this).attr("href") + "']");
+		//alert(linktoclick);
+		//alert(   linktoclick[0].outerHTML );
+		$(".clickeditem").removeClass("clickeditem");
+		$(linktoclick).toggleClass("clickeditem");
+	});
 
-            // .innerlink class allows links in one main details to open links within another main details even if that main details is closed.
-            $(".innerlink").on("click", function(event) {
-                //if closed and inner link open details first then trigger click
-                // TODO: Not checked for links to details within details
-                if (!$($(this).attr("href")).parents("details").last().prop("open")) {
-                    $($(this).attr("href")).parents("details").last().find("summary").trigger("click");
-                    $($(this).attr("href")).trigger("click");
-                };
-                //alert("2");
-            });
-            // END DETAILS SECTION
+	// handle external links to new tab
+	$("a")
+		.filter(function () {
+			return this.hostname && this.hostname !== location.hostname;
+		})
+		.addClass("external")
+		.attr({
+			target: "_blank",
+			title: function () {
+				return "Open in new window: " + $(this).attr("title");
+			},
+		});
+	//.append(' [^]');
 
-            // ANCHOR SECTION modify anchors 
+	// END ANCHOR SECTION
 
-
-            // Don't follow empty links, like footnotes
-            $('a[href="#"]').on("click", function(event) {
-                //alert("3");
-                return false;
-            });
-
-            // /* when link to a target in the page, i.e. #.. is clicked, the link is highlighted via jquery. Used to show which picture is showing in the galleries. see style sheet a.clickeditem */
-            $('a[href*="#"]').click(function(){
-            //alert("clicked" + $(this).attr("href") );
-            //$(this).attr("href");
-            var linktoclick = $("a[href='" + $(this).attr("href") + "']");
-            //alert(linktoclick);
-            //alert(   linktoclick[0].outerHTML );
-            $(".clickeditem").removeClass("clickeditem");
-            $(linktoclick).toggleClass("clickeditem");
-            });
-
-            // handle external links to new tab
-            $('a')
-                .filter(function() {
-                return this.hostname && this.hostname !== location.hostname;
-                })
-                .addClass("external")
-                .attr({
-                    target: "_blank",
-                    title: function() { return "Open in new window: " + $(this).attr("title") }
-                    });
-                //.append(' [^]');
-
-            // END ANCHOR SECTION
-
-
-            // SCRATCHBLOCKS: in concert with scratchblocks-v3.5-min.js renders beautiful scratchblocks from simple markdown. Hooray!
-            // use as:
-            //``` pre ```
-            //{: .msb}
-            scratchblocks.renderMatching('.msb pre', {
-                style: 'scratch3',
-                languages: ['en'],
-            });
-            scratchblocks.renderMatching('code.msb', {
-                style: 'scratch3',
-                languages: ['en'],
-            });
-
-        });
+	// SCRATCHBLOCKS: in concert with scratchblocks-v3.5-min.js renders beautiful scratchblocks from simple markdown. Hooray!
+	// use as:
+	//``` pre ```
+	//{: .msb}
+	scratchblocks.renderMatching(".msb pre", {
+		style: "scratch3",
+		languages: ["en"],
+	});
+	scratchblocks.renderMatching("code.msb", {
+		style: "scratch3",
+		languages: ["en"],
+	});
+});
---
+++ junior_coders/0c23f5f29d83139f13fc32a19c49dee01bfcd93f/class_notes_lessons/testzakLazy.md
@@ -0,0 +1,35 @@
+---
+layout: junior_coders_default
+title: Getting Started with Scratch Jr. (Monday PM)
+title2: 
+levelsToTop: "../"
+tags: []
+---
+
+
+
+
+<details markdown=1>
+<summary markdown=1>## xxx
+</summary>
+
+## xxx
+
+### Homework due xxx
+
+### Recap for xxx
+
+
+{% include zakviewerNoLazyLoad.html Name="test" ID="https://scratch.mit.edu/projects/610086538/" caption="test" %}
+
+
+{% include zakviewerNoLazyLoad.html Name="test" ID="https://scratch.mit.edu/projects/347317923/" caption="test" %}
+
+
+{% include zakviewer.html Name="test" ID="https://scratch.mit.edu/projects/610086538/" caption="test" %}
+
+
+{% include zakviewer.html Name="test" ID="https://scratch.mit.edu/projects/347317923/" caption="test" %}
+
+
+</details>
